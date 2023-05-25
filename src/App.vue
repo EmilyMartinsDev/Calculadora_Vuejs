f85afaeead673f50d1a3774c27c52f8a51eeef84
emily <emilycaroline01042005@gmail.com>
2023-05-25 15:44:04 -0300

    construcao logica

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+
+const estado = reactive({
+  primeiroNumero: '',
+  segundoNumero:'',
+  calculo:'',
+  resultado:'',
+});
+
+function geraResultado(){
+const {primeiroNumero, segundoNumero, calculo} = estado;
+
+if(!primeiroNumero || !segundoNumero){
+  return
+}
+
+switch (calculo){
+  case 'multiplicar':
+    return Number(primeiroNumero) * Number(segundoNumero);
+  case 'dividir':
+    return (Number(primeiroNumero) / Number(segundoNumero)).toFixed(2);
+  case 'somar':
+    return Number(primeiroNumero) + Number(segundoNumero);
+  case 'subtrair':
+    return Number(primeiroNumero) - Number(segundoNumero);
+ 
+}
+
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <div class="container mt-5 bg-light">
+  <header class="p-4 bg-dark text-white mb-5 text-center">
+    <h1>Calculadora Aritmédica</h1>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <form action="">
+      <div class="row">
+        <div class="col-md-3">
+          <input class="form-control" @change="(e)=>estado.primeiroNumero = e.target.value" type="number" placeholder="Primeiro numero">
+        </div>
+        <div class="col-md-3">
+          <input class="form-control" @change="(e)=>estado.segundoNumero = e.target.value" type="number" placeholder="Segundo numero">
+        </div>
+        <div class="col-md-6">
+          <select @change="(e)=>estado.calculo = e.target.value" class="form-control">
+            <option value="multiplicar">multiplicar</option>
+            <option value="dividir">dividir</option>
+            <option value="somar">somar</option>
+            <option value="subtrair">subtrair</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <div  class="p-3 bg-success mt-5 display-4 text-center text-light">
+      <span>Resultado:</span>
+    <span>{{ geraResultado() }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.form{
+  background-color: #ccc;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.isInvalid{
+  background-color: red;
 }
 </style>
